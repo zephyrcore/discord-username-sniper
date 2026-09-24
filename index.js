905582c3965b83645853e22af2914dc206f332f4
Zephyr <ricodxxbb@gmail.com>
2026-09-24 23:26:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import 'envparse2/config';
+import 'better-dotenv3/config';
 import fs from 'fs';
 import { checkUsername } from './utils/check.js';
 import { claimUsername } from './utils/claim.js';
